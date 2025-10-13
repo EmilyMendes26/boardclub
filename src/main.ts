c902d4326836a98e2a632b828dbd6fba811ae062
Saymor225 <jsilva.neto2002@gmail.com>
2025-10-13 05:32:48 -0300

    Validação do usuário
    O usuário cliente agora é obrigado a  seguir algumas regras de parâmetro:
    Nome : Não pode ser vazio
    Email: Não Pode ser vazio e deve possui sufixo de email (@exemplo.com)
    Password: Senha deve ser forte com pelo menos 8 caracteres (deve conter no mínimo: 1 letra maiúscula, 1 minúscula, 1 carácter especial (.,!?%), 1 numero)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Ative os pipes de validação ANTES de iniciar o servidor
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+      transformOptions: { enableImplicitConversion: false },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsNotEmpty, IsEmail, isStrongPassword, IsStrongPassword } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'The name field should not be empty.' })
   nome: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword()
   password: string;
 }
